0fee50c203eda4a7fa959458106a1b5ac8fe4e9e
nig <nig@statsbiblioteket.dk>
2017-09-12 09:43:45 +0200

    Prettier plugin added

--- app/js/vueapp.js
+++ app/js/vueapp.js
@@ -1,121 +1,135 @@
 /* External Vue stuff */
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-import Vuex from 'vuex'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import VueResource from "vue-resource";
+import Vuex from "vuex";
 
 /* Vue plugins  */
-//import createPersistedState from 'vuex-persistedstate'; //saves state to local storage to keep state at reload 
+//import createPersistedState from 'vuex-persistedstate'; //saves state to local storage to keep state at reload
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 Vue.use(Vuex);
 
 /* Internal Vue stuff */
-import { Title, Stamps, Dice, Foo, Bar, Page404, Routeinfo, BarHome, BarBar, BarFoo } from './vuecomponents.js';
-import { timeFilter } from './vuefilters.js';
+import {
+  Title,
+  Stamps,
+  Dice,
+  Foo,
+  Bar,
+  Page404,
+  Routeinfo,
+  BarHome,
+  BarBar,
+  BarFoo
+} from "./vuecomponents.js";
+import { timeFilter } from "./vuefilters.js";
 
-Vue.filter('toLocaleTime', timeFilter)
-Vue.component('header-box', Title);
-Vue.component('routeinfo-box', Routeinfo);
+Vue.filter("toLocaleTime", timeFilter);
+Vue.component("header-box", Title);
+Vue.component("routeinfo-box", Routeinfo);
 
 /* Vanilla JS function */
-import { helloHelper } from './helperFunctions.js'
+import { helloHelper } from "./helperFunctions.js";
 
 const router = new VueRouter({
-    //mode: 'history', //Reload doesn't work with 'history mode'
-    base: __dirname,
-    linkActiveClass: 'hey-i-am-active',
-    routes: [
-        { path: '/home', component: Stamps, alias: '/stamps' },
-        { path: '/', redirect: '/home' },
-        { path: '/dice', component: Dice },
-        { path: '/foo', components: { default: Foo, footer: Bar }, props: { default: { someArray2: [13, 17, 19, 23, 27] } } },
+  //mode: 'history', //Reload doesn't work with 'history mode'
+  base: __dirname,
+  linkActiveClass: "hey-i-am-active",
+  routes: [
+    { path: "/home", component: Stamps, alias: "/stamps" },
+    { path: "/", redirect: "/home" },
+    { path: "/dice", component: Dice },
+    {
+      path: "/foo",
+      components: { default: Foo, footer: Bar },
+      props: { default: { someArray2: [13, 17, 19, 23, 27] } }
+    },
+    {
+      path: "/bar",
+      components: { default: Bar, footer: Foo },
+      redirect: "/bar/home",
+      children: [
         {
-            path: '/bar',
-            components: { default: Bar, footer: Foo },
-            redirect: '/bar/home',
-            children: [{
-                    path: 'home',
-                    component: BarHome
-                },
-                {
-                    path: 'foo',
-                    component: BarFoo
-                },
-                {
-                    path: 'bar',
-                    component: BarBar
-                }
-            ]
+          path: "home",
+          component: BarHome
         },
-        { path: '/404', component: Page404 },
-        { path: '*', redirect: '/404' }
-    ]
+        {
+          path: "foo",
+          component: BarFoo
+        },
+        {
+          path: "bar",
+          component: BarBar
+        }
+      ]
+    },
+    { path: "/404", component: Page404 },
+    { path: "*", redirect: "/404" }
+  ]
 });
 
 const store = new Vuex.Store({
-    //plugins: [createPersistedState()], //saves state to local storage to keep state at reload 
-    state: {
-        count: 0,
-        routechange: 0,
-        user: '',
-        diceObj: {
-            vuedice: {
-                outcome: '?',
-                rolls: 0,
-                total: 0,
-            },
-            vanilladice: {
-                outcome: '?',
-                rolls: 0,
-                total: 0,
-            }
-        }
-    },
-    mutations: {
-        plus10(state) {
-            state.count = state.count + 10;
-        },
-        resetcounter(state) {
-            state.count = 0;
-        },
-        setUser(state, user) {
-            state.user = user;
-        },
-        updateDice(state, diceObj) {
-            state.diceObj = diceObj
-        },
-        resetDice(state) {
-            state.diceObj = {
-                vuedice: {
-                    outcome: '?',
-                    rolls: 0,
-                    total: 0,
-                },
-                vanilladice: {
-                    outcome: '?',
-                    rolls: 0,
-                    total: 0,
-                }
-            }
-        }
+  //plugins: [createPersistedState()], //saves state to local storage to keep state at reload
+  state: {
+    count: 0,
+    routechange: 0,
+    user: "",
+    diceObj: {
+      vuedice: {
+        outcome: "?",
+        rolls: 0,
+        total: 0
+      },
+      vanilladice: {
+        outcome: "?",
+        rolls: 0,
+        total: 0
+      }
     }
-})
-
-
+  },
+  mutations: {
+    plus10(state) {
+      state.count = state.count + 10;
+    },
+    resetcounter(state) {
+      state.count = 0;
+    },
+    setUser(state, user) {
+      state.user = user;
+    },
+    updateDice(state, diceObj) {
+      state.diceObj = diceObj;
+    },
+    resetDice(state) {
+      state.diceObj = {
+        vuedice: {
+          outcome: "?",
+          rolls: 0,
+          total: 0
+        },
+        vanilladice: {
+          outcome: "?",
+          rolls: 0,
+          total: 0
+        }
+      };
+    }
+  }
+});
 
 const VueApp = new Vue({
-    router,
-    store,
-    el: '#app',
-    data: {
-        stamps: {},
-        user: '',
-        someArray: [2, 3, 5, 7, 11, 13, 13, 13, 17, 19],
-        title: 'Testing vue, webpack and ecma script 6 imports',
-    },
-    template: `
+  router,
+  store,
+  el: "#app",
+  data: {
+    stamps: {},
+    user: "",
+    someArray: [2, 3, 5, 7, 11, 13, 13, 13, 17, 19],
+    title: "Testing vue, webpack and ecma script 6 imports"
+  },
+  template: `
         <div class="wrapper"> 
             <header-box :title='title'></header-box>
             <ul id="menu">
@@ -134,9 +148,9 @@ const VueApp = new Vue({
             <router-view class="view 2" id="footer" name="footer"></router-view>
         </div>
     `,
-    created: function() {
-        helloHelper('from "VueApp" in vueapp.js');
-    }
-})
+  created: function() {
+    helloHelper('from "VueApp" in vueapp.js');
+  }
+});
 
 export { VueApp, router, store };
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,42 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    //entry: './app/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        proxy: {
-            '/spot-service': {
-                target: 'http://devel06:8381',
-                pathRewrite: { '^/spot-service': '' },
-                secure: false
-            }
-        }
-    },
-    entry: {
-        app: './app/js/index.js',
-        helperFunctions: './app/js/helperFunctions.js'
-    },
-    output: {
-        //filename: 'bundle.js',
-        filename: './js/[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js'
-        }
-    },
-    module: {
-        rules: [{
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            }
-        ]
+  //entry: './app/index.js',
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    proxy: {
+      "/spot-service": {
+        target: "http://devel06:8381",
+        pathRewrite: { "^/spot-service": "" },
+        secure: false
+      }
     }
+  },
+  entry: {
+    app: "./app/js/index.js",
+    helperFunctions: "./app/js/helperFunctions.js"
+  },
+  output: {
+    //filename: 'bundle.js',
+    filename: "./js/[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
 };
